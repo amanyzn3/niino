3ed4b9e7b8d216c2e8898fa77c393286e9ad220e
amanyzn3 <amanyzn3@gmail.com>
2026-02-17 17:04:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["nino-care.onrender.com"],
     proxy: {
       "/api": {
         target: "http://localhost:3000",
